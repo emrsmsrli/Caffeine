a665f81028013291c0f130fc9807799896533283
Emre <emrsmsrli1@gmail.com>
2017-07-12 18:06:21 +0300

    integrate timer in clock object

--- app/src/main/java/tr/edu/iyte/caffeine/util/Clock.kt
+++ app/src/main/java/tr/edu/iyte/caffeine/util/Clock.kt
@@ -1,11 +1,18 @@
 package tr.edu.iyte.caffeine.util
 
+import android.os.CountDownTimer
+import org.jetbrains.anko.AnkoLogger
+
 object Clock {
-    private val THIRTY_THREE = 33
-    private val SIXTY_SIX = 66
+    val FULL = 100
+    val THIRTY_THREE = 33
+    val SIXTY_SIX = 66
 
     private var min: Int = 0
     private var sec: Int = 0
+
+    private var timer: TimerObject.Timer? = null
+    var listener: ClockListener? = null
 
     /**
      * Sets [Clock.min] to the [min], [Clock.sec] to 0.
@@ -14,6 +21,19 @@ object Clock {
     fun set(min: Int) {
         Clock.min = min
         sec = 0
+        timer?.cancel()
+        timer = TimerObject.Timer(min)
+        timer?.start()
+    }
+
+    /**
+     * Resets the clock
+     */
+    fun reset() {
+        min = 0
+        sec = 0
+        timer?.cancel()
+        listener?.onFinish()
     }
 
     /**
@@ -39,21 +59,41 @@ object Clock {
      */
     fun getPercentage(): Int {
         val totalsec = min * 60 + sec
-        val perc = totalsec.toFloat() / /*(CaffeineManager.mode.min * 60) * */ 100
+        val perc = totalsec.toFloat() / (CaffeineManager.mode.min * 60) * 100
         if (perc <= THIRTY_THREE)
             return THIRTY_THREE
         else if (perc <= SIXTY_SIX)
             return SIXTY_SIX
         else
-            return 0
+            return FULL
     }
 
     /**
      * @return A human readable form of a [Clock] object. Format is always *(m)m:ss*.
      */
     override fun toString(): String {
+        if(CaffeineManager.mode == CaffeineMode.INFINITE_MINS)
+            return CaffeineManager.mode.label
         if (sec < 10)
             return "$min:0$sec"
         return "$min:$sec"
+    }
+
+    internal object TimerObject {
+        const val SEC_IN_MILLIS = 1000L
+        const val MIN_IN_MILLIS = 60 * SEC_IN_MILLIS
+
+        class Timer(min: Int) :
+                CountDownTimer(min * MIN_IN_MILLIS, SEC_IN_MILLIS), AnkoLogger {
+            override fun onTick(millisUntilFinished: Long) {
+                decrement()
+                listener?.onTick()
+            }
+
+            override fun onFinish() {
+                CaffeineManager.reset()
+                listener?.onFinish()
+            }
+        }
     }
 }
