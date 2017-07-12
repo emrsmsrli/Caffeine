7d5d88ab6f695658bd0ea76661e5f772eb08d122
Emre <emrsmsrli1@gmail.com>
2017-07-12 19:34:22 +0300

    caffeineManager now handles wakelock acquiring releasing operations

--- app/src/main/java/tr/edu/iyte/caffeine/util/CaffeineManager.kt
+++ app/src/main/java/tr/edu/iyte/caffeine/util/CaffeineManager.kt
@@ -1,7 +1,17 @@
 package tr.edu.iyte.caffeine.util
 
-object CaffeineManager {
+import android.content.Context
+import android.content.Intent
+import android.os.PowerManager
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.error
+import org.jetbrains.anko.info
+import org.jetbrains.anko.startService
+import tr.edu.iyte.caffeine.services.ScreenOffReceiverService
 
+object CaffeineManager : AnkoLogger {
+    private var wakeLock: PowerManager.WakeLock? = null
+    var context: Context? = null
     var mode = CaffeineMode.INACTIVE
         private set
 
@@ -13,9 +23,10 @@ object CaffeineManager {
             CaffeineMode.TEN_MINS -> {
                 mode = mode.next()
                 Clock.set(mode.min)
+                acquireWakeLock()
             }
             CaffeineMode.INFINITE_MINS -> {
-                mode = mode.next()
+                reset()
                 Clock.reset()
             }
         }
@@ -23,6 +34,37 @@ object CaffeineManager {
 
     fun reset() {
         mode = CaffeineMode.INACTIVE
+        context?.stopService(Intent(context, ScreenOffReceiverService::class.java))
+        releaseWakeLock()
     }
 
+    @Suppress("deprecation")
+    fun acquireWakeLock() {
+        if(wakeLock == null) {
+            info("Acquiring wakelock..")
+            val powerManager = context?.getSystemService(Context.POWER_SERVICE) as? PowerManager
+            if(powerManager == null) {
+                Clock.reset()
+                reset()
+                return
+            }
+
+            wakeLock = powerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "CaffeineWL")
+            wakeLock?.acquire()
+
+            if(context?.startService<ScreenOffReceiverService>() == null) {
+                error("Cannot start ScreenOffReceiverService, Caffeine won't continue")
+                Clock.reset()
+                reset()
+            }
+        }
+    }
+
+    fun releaseWakeLock() {
+        if(wakeLock != null && wakeLock!!.isHeld) {
+            info("Releasing wakelock..")
+            wakeLock?.release()
+            wakeLock = null
+        }
+    }
 }
